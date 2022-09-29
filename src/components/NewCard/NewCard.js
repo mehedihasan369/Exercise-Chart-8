1df5f2fd28943addf468262a09ffe779d841cd60
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-09-29 23:11:03 +0600

    component

--- src/components/NewCard/NewCard.js
+++ src/components/NewCard/NewCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './NewCard.css'
-const NewCard = () => {
+const NewCard = (props) => {
+    const {name,img , time} = props.fullData
     return (
-        <div>
+        <div className='img-bg   col-3  m-1 p-2'>
+            <img src={img} alt="" className='w-100 h-75' />
+            <div className='w-100 h-50'>
+            <h3>{name}</h3>
+            <h5>Time required : {time}</h5>
             
+            </div>
+            <button className='btn btn-outline-success text-dark fw-bold '>Add to list</button>
+
         </div>
     );
 };
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NewCard from '../NewCard/NewCard';
 import './cards.css'
 
 const Cards = () => {
+  
+    const [data,setData] = useState([]);
+
+    useEffect( () =>{
+        fetch('./data.JSON')
+        .then(res=> res.json())
+        .then(data => setData(data))
+    }, []);
+
     return (
         <div className='cardcompo container'>
             <h1 className='text-success'>Exercise Chart</h1>
              <h3>See todays exercise...</h3>
-             <div>
-                <NewCard></NewCard>
+             <div className='cards row '>
+                {data.map(data => <NewCard  key={data.id} fullData={data}></NewCard>)}
+               
              </div>
         </div>
     );
-};
+}
 
 
 
